30d06811879b60c0ba239b757fdb7098e8f93505
aleksajaw <jaworska.aleksa@gmail.com>
2024-01-06 18:41:53 +0100

    The initial commit with added basics for a cart

---
+++ resources/views/cart.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            Hello World, we are in the cart now!
+        </div>
+    </div>
+</div>
+@endsection
